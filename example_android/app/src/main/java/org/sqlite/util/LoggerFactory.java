7e1b07fcab418423c698b3814b9977901e2e441e
zoff99 <zoff99@users.noreply.github.com>
2025-01-04 10:16:24 +0100

    completely remove useless dependency on slf4j

--- example_android/app/src/main/java/org/sqlite/util/LoggerFactory.java
+++ example_android/app/src/main/java/org/sqlite/util/LoggerFactory.java
@@ -10,18 +10,7 @@ import java.util.logging.Level;
  * java.util.logging implementation otherwise.
  */
 public class LoggerFactory {
-    static final boolean USE_SLF4J;
-
-    static {
-        boolean useSLF4J;
-        try {
-            Class.forName("org.slf4j.Logger");
-            useSLF4J = true;
-        } catch (Exception e) {
-            useSLF4J = false;
-        }
-        USE_SLF4J = useSLF4J;
-    }
+    static final boolean USE_SLF4J = false;
 
     /**
      * Get a {@link Logger} instance for the given host class.
@@ -30,10 +19,6 @@ public class LoggerFactory {
      * @return a Logger
      */
     public static Logger getLogger(Class<?> hostClass) {
-        if (USE_SLF4J) {
-            return new SLF4JLogger(hostClass);
-        }
-
         return new JDKLogger(hostClass);
     }
 
@@ -80,54 +65,6 @@ public class LoggerFactory {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
                 {
                     logger.log(Level.SEVERE, message.get(), t);
-                }
-            }
-        }
-    }
-
-    private static class SLF4JLogger implements Logger {
-        final org.slf4j.Logger logger;
-
-        SLF4JLogger(Class<?> hostClass) {
-            logger = org.slf4j.LoggerFactory.getLogger(hostClass);
-        }
-
-        @Override
-        public void trace(Supplier<String> message) {
-            if (logger.isTraceEnabled()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                {
-                    logger.trace(message.get());
-                }
-            }
-        }
-
-        @Override
-        public void info(Supplier<String> message) {
-            if (logger.isInfoEnabled()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                {
-                    logger.info(message.get());
-                }
-            }
-        }
-
-        @Override
-        public void warn(Supplier<String> message) {
-            if (logger.isWarnEnabled()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                {
-                    logger.warn(message.get());
-                }
-            }
-        }
-
-        @Override
-        public void error(Supplier<String> message, Throwable t) {
-            if (logger.isErrorEnabled()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-                {
-                    logger.error(message.get(), t);
                 }
             }
         }
