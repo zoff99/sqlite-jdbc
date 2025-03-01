323fc72ae91c1eee3b89ac989f62aa652b1d2b50
zoff99 <zoff99@users.noreply.github.com>
2025-03-01 10:04:15 +0100

    jitpack lib 1.0.2

--- example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java
+++ example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java
@@ -2,7 +2,7 @@ package com.zoffcc.applications.androidjdbc;
 
 public final class BuildConfig {
 
-    public static final int VERSION_CODE = 10001;
-    public static final String VERSION_NAME = "1.0.1";
+    public static final int VERSION_CODE = 10002;
+    public static final String VERSION_NAME = "1.0.2";
 
 }
