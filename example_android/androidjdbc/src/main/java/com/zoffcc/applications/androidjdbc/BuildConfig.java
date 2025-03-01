b4eae4b513faba136e32c9f7b9fe34b1453827da
zoff99 <zoff99@users.noreply.github.com>
2025-03-01 10:03:06 +0100

    add manual BuildConfig class

---
+++ example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java
@@ -0,0 +1,8 @@
+package com.zoffcc.applications.androidjdbc;
+
+public final class BuildConfig {
+
+    public static final int VERSION_CODE = 10001;
+    public static final String VERSION_NAME = "1.0.1";
+
+}
