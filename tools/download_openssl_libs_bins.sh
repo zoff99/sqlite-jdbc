4d76328bbf881bb15a61fc6d10268af06690dc7e
zoff99 <zoff99@users.noreply.github.com>
2025-06-13 08:51:34 +0200

    fix urls to actually download the binary files

--- tools/download_openssl_libs_bins.sh
+++ tools/download_openssl_libs_bins.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-url_linux_crypto="https://github.com/zoff99/iocipher_pack/blob/master/002_src_libsqlfs/openssl_libs/libcrypto.a"
-url_linux_ssl="https://github.com/zoff99/iocipher_pack/blob/master/002_src_libsqlfs/openssl_libs/libssl.a"
+url_linux_crypto="https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/002_src_libsqlfs/openssl_libs/libcrypto.a"
+url_linux_ssl="https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/002_src_libsqlfs/openssl_libs/libssl.a"
 
 _HOME2_=$(dirname $0)
 export _HOME2_
@@ -15,17 +15,17 @@ mkdir -p "$basedir""/openssl_libs/"
 wget "$url_linux_crypto" -O ./openssl_libs/Linux-x86_64/libcrypto.a || exit 1
 wget "$url_linux_ssl" -O ./openssl_libs/Linux-x86_64/libssl.a || exit 1
 
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/libcrypto.a -O ./openssl_libs/android-arm64/libcrypto.a || exit 1
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/libssl.a -O ./openssl_libs/android-arm64/libssl.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/libcrypto.a -O ./openssl_libs/android-arm64/libcrypto.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/libssl.a -O ./openssl_libs/android-arm64/libssl.a || exit 1
 
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/libcrypto.a -O ./openssl_libs/android-arm/libcrypto.a || exit 1
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/libssl.a -O ./openssl_libs/android-arm/libssl.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/libcrypto.a -O ./openssl_libs/android-arm/libcrypto.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/libssl.a -O ./openssl_libs/android-arm/libssl.a || exit 1
 
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/libcrypto.a -O ./openssl_libs/android-x86/libcrypto.a || exit 1
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/libssl.a -O ./openssl_libs/android-x86/libssl.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/libcrypto.a -O ./openssl_libs/android-x86/libcrypto.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/libssl.a -O ./openssl_libs/android-x86/libssl.a || exit 1
 
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/libcrypto.a -O ./openssl_libs/android-x86_64/libcrypto.a || exit 1
-wget https://github.com/zoff99/iocipher_pack/blob/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/libssl.a -O ./openssl_libs/android-x86_64/libssl.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/libcrypto.a -O ./openssl_libs/android-x86_64/libcrypto.a || exit 1
+wget https://github.com/zoff99/iocipher_pack/raw/refs/heads/master/003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/libssl.a -O ./openssl_libs/android-x86_64/libssl.a || exit 1
 
 
 mkdir -p "$basedir""/openssl_includes/temp/"
