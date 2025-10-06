1710e29d55486158873dcee2296b11469b461e31
zoff99 <zoff99@users.noreply.github.com>
2025-10-06 20:53:07 +0200

    build maven artefact

--- example_linux/do_compile.sh
+++ example_linux/do_compile.sh
@@ -6,7 +6,7 @@ _HOME_=$(cd $_HOME2_;pwd)
 export _HOME_
 
 basedir="$_HOME_""/"
-cd "$basedir"
+cd "$basedir" || exit 1
 pwd
 
 ## get current version ------------------------
@@ -20,15 +20,18 @@ echo "$cur_num_version"
 echo "$cur_str_version"
 ## get current version ------------------------
 
-cd "$basedir"
+cd "$basedir" || exit 1
 
 rm -Rf _jar/
-mkdir _jar/
-cd _jar/
+mkdir _jar/ || exit 1
+cd _jar/ || exit 1
 
-cp -a ../_android/android .
+
+echo "create jar artefact"
+
+cp -a ../_android/android . || exit 1
 ## not needed for desktop java ## cp -av ../../example_android/androidjdbc/src/main/java/java .
-cp -a ../../example_android/androidjdbc/src/main/java/org .
+cp -a ../../example_android/androidjdbc/src/main/java/org . || exit 1
 
 find . -name '*.java' | xargs -L10000 javac -nowarn || exit 1
 find . -name '*.java' | xargs -L10000 rm
@@ -37,16 +40,35 @@ mkdir -p META-INF
 echo 'Manifest-Version: 1.0
 Bundle-Description: SQLite JDBC library (jdbcjni)
 Bundle-Name: SQLite JDBC (jdbcjni)
-Built-By: Zoff' > META-INF/MANIFEST.MF
+Built-By: Zoff' > META-INF/MANIFEST.MF || exit 1
 
 mkdir -p META-INF/services/
-echo 'org.sqlite.JDBC' > META-INF/services/java.sql.Driver
+echo 'org.sqlite.JDBC' > META-INF/services/java.sql.Driver || exit 1
 
 mkdir -p org/sqlite/native/Linux/x86_64
-cp -av ../../src/main/resources/org/sqlite/native/Linux/x86_64/libsqlitejdbc.so org/sqlite/native/Linux/x86_64/libsqlitejdbc.so
+cp -av ../../src/main/resources/org/sqlite/native/Linux/x86_64/libsqlitejdbc.so org/sqlite/native/Linux/x86_64/libsqlitejdbc.so || exit 1
 
-jar cmvf META-INF/MANIFEST.MF ../sqlite-jdbc-sqlcipher-"$cur_str_version".jar .
+jar cmvf META-INF/MANIFEST.MF ../sqlite-jdbc-sqlcipher-"$cur_str_version".jar . || exit 1
 
 cd ../
-ls -al sqlite-jdbc-sqlcipher-"$cur_str_version".jar
-ls -hal sqlite-jdbc-sqlcipher-"$cur_str_version".jar
+ls -al sqlite-jdbc-sqlcipher-"$cur_str_version".jar || exit 1
+ls -hal sqlite-jdbc-sqlcipher-"$cur_str_version".jar || exit 1
+
+echo "create maven artefact"
+
+rm -Rf /.m2/
+
+mvn install:install-file \
+  -Dfile=sqlite-jdbc-sqlcipher-"$cur_str_version".jar \
+  -DgroupId=com.zoffcc.applications.sqlitejdbc \
+  -DartifactId=sqlite-jdbc-sqlcipher \
+  -Dversion="$cur_str_version" \
+  -Dpackaging=jar \
+  -DlocalRepositoryPath="$basedir"/../example_linux/.m2/repository/ \
+  -DgeneratePom=true
+
+rm -f local_maven_sqlitejdbc_*.zip
+zip -r local_maven_sqlitejdbc_"$cur_str_version".zip ./.m2 || exit 1
+
+ls -al local_maven_sqlitejdbc_"$cur_str_version".zip || exit 1
+ls -hal local_maven_sqlitejdbc_"$cur_str_version".zip || exit 1
