69a0419ca767d04f2ed2ad00f54884f67dcd2232
zoff99 <zoff99@users.noreply.github.com>
2025-10-10 19:25:40 +0200

    remove unneeded setting of driver class name

--- example_linux/Sample.java
+++ example_linux/Sample.java
@@ -11,14 +11,6 @@
 
       public static void main(String[] args)
       {
-        try
-        {
-            Class.forName("org.sqlite.JDBC");
-        }
-        catch(Exception e)
-        {
-        }
-
         // NOTE: Connection and Statement are AutoCloseable.
         //       Don't forget to close them both in order to avoid leaks.
         try
