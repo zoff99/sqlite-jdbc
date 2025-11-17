8b6a1ce4a570fc8412f7c257684a98b996e7e7c9
zoff99 <zoff99@users.noreply.github.com>
2025-11-17 17:23:16 +0100

    update also version used in gradle example

--- tools/maven_jniartefact_prepare_new_release_version.sh
+++ tools/maven_jniartefact_prepare_new_release_version.sh
@@ -9,6 +9,7 @@ basedir="$_HOME_""/../"
 
 f1="example_android/androidjdbc/build.gradle"
 f2="example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java"
+f3='example_gradle_linux/app/build.gradle'
 
 cd "$basedir"
 
@@ -42,8 +43,10 @@ sed -i -e 's#versionName ".*#versionName "'"$next_m_version"'"#g' "$f1"
 sed -i -e 's#public static final int VERSION_CODE = .*$#public static final int VERSION_CODE = '"$next_p_version"';#g' "$f2"
 sed -i -e 's#public static final String VERSION_NAME = ".*$#public static final String VERSION_NAME = "'"$next_m_version"'";#g' "$f2"
 
+sed -i -e 's#pkgs_zoffcc_sqlite-jdbc-sqlcipher:.*#pkgs_zoffcc_sqlite-jdbc-sqlcipher:'"$next_m_version""'"'#g' "$f3"
+
 commit_message="jni ""$next_m_version"
 tag_name="jdbcjni""$next_m_version"
 
-git commit -m "$commit_message" "$f1" "$f2"
+git commit -m "$commit_message" "$f1" "$f2" "$f3"
 ## do not tag yet ## git tag -a "$tag_name" -m "$tag_name"
