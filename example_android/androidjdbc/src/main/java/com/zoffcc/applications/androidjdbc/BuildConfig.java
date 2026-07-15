7ab0b02dead4162da9f681d66879b638faff9a55
zoff99 <zoff99@users.noreply.github.com>
2026-07-15 15:34:56 +0200

    jni 1.0.28

--- example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java
+++ example_android/androidjdbc/src/main/java/com/zoffcc/applications/androidjdbc/BuildConfig.java
@@ -2,7 +2,7 @@ package com.zoffcc.applications.androidjdbc;
 
 public final class BuildConfig {
 
-    public static final int VERSION_CODE = 10027;
-    public static final String VERSION_NAME = "1.0.27";
+    public static final int VERSION_CODE = 10028;
+    public static final String VERSION_NAME = "1.0.28";
 
 }
